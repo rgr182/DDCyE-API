09b97111432b8ecc06635a0a883b44b4d03381f9
RAULPC\rgr_1 <rgr182>
2024-05-10 17:57:24 -0600

    HEHE

--- DDEyC API/Program.cs
+++ DDEyC API/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Volviendo A Casa Auth", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Auth", Version = "v1" });
 });
 
 // Register your services
