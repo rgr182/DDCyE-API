2800dade0ea8ed25ee08d50c9d7a729250a2f7b2
RAULPC\rgr_1 <rgr182>
2024-08-27 15:51:13 -0600

    Missing Files

--- Cron-BolsaDeTrabajo/obj/Debug/net6.0/Cron-BolsaDeTrabajo.AssemblyInfo.cs
+++ Cron-BolsaDeTrabajo/obj/Debug/net6.0/Cron-BolsaDeTrabajo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cron-BolsaDeTrabajo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+639e839fc112b3a3f8bea0b4afd4d2d62b3f9c36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cron-BolsaDeTrabajo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cron-BolsaDeTrabajo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
