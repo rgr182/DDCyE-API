f9a9843e6e0174195ed5ebd2bad7a31dc2e684cd
RAULPC\rgr_1 <rgr182>
2024-08-27 16:03:30 -0600

    Build fixed

---
+++ Cron-DDEyC/Services/ApiService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Cron_DDEyC.Services
+{
+    public class ApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<string> CallApiAsync(string url)
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                return await response.Content.ReadAsStringAsync();
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"HTTP Request error: {e.Message}");
+                return null;
+            }
+        }
+    }
+}
