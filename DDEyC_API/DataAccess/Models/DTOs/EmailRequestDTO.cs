5d92a123cb0e87a63e819b768a009dc49c59e18a
Daniel M. <failniel04@gmail.com>
2024-09-17 16:50:46 -0600

    codefactor scolded me

--- DDEyC_API/DataAccess/Models/DTOs/EmailRequestDTO.cs
+++ DDEyC_API/DataAccess/Models/DTOs/EmailRequestDTO.cs
@@ -10,11 +10,5 @@ namespace DDEyC_API.DataAccess.Models.DTOs
         public List<EmailAttachment> Attachments { get; set; } = new List<EmailAttachment>();
     }
 
-    public class EmailAttachment
-    {
-        public string FileName { get; set; }
-        public string ContentId { get; set; }
-        public byte[] Content { get; set; }
-        public string MimeType { get; set; }
-    }
+   
 }
