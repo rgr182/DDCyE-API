30d1da09dcb851b9bf1ffcc61f083d639a552186
Daniel M. <failniel04@gmail.com>
2024-09-26 15:02:45 -0600

    Fixed issue with timestamps not appearing correctly

--- DDEyC-Assistant/Models/DTOs/MessageDTO.cs
+++ DDEyC-Assistant/Models/DTOs/MessageDTO.cs
@@ -1,9 +1,12 @@
- 
- namespace DDEyC_Assistant.Models.DTOs{
+
+using System.Text.Json.Serialization;
+
+namespace DDEyC_Assistant.Models.DTOs{
 public class MessageDto
     {
         public string Content { get; set; }
         public string Role { get; set; }
+         [JsonPropertyName("timestamp")]
         public DateTime Timestamp { get; set; }
     }
  }
