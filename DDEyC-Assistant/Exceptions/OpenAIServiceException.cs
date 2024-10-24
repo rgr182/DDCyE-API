6750c362f27671c44fa1dd50b7ee5d0a51fbd737
Daniel M. <failniel04@gmail.com>
2024-10-24 16:26:11 -0600

    Codefactor whining fix

--- DDEyC-Assistant/Exceptions/OpenAIServiceException.cs
+++ DDEyC-Assistant/Exceptions/OpenAIServiceException.cs
@@ -1,21 +1,5 @@
 namespace DDEyC_Assistant.Exceptions
 {
-    public class ChatServiceException : Exception
-    {
-        public string ErrorCode { get; }
-
-        public ChatServiceException(string message, string errorCode) : base(message)
-        {
-            ErrorCode = errorCode;
-        }
-
-        public ChatServiceException(string message, string errorCode, Exception innerException) 
-            : base(message, innerException)
-        {
-            ErrorCode = errorCode;
-        }
-    }
-
     public class OpenAIServiceException : ChatServiceException
     {
         public OpenAIServiceException(string message, string errorCode) 
